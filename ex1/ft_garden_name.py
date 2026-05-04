4a189c9725688eac8b5fc943865f1b08ba11f99a
fanilran <fanilran@student.42antananari>
2026-05-04 12:04:29 +0300

    ex1

---
+++ ex1/ft_garden_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_garden_name.py                                    :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: fanilran <fanilran@student.42.fr>            +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/03/28 10:51:14 by fanilran            #+#    #+#            #
+#   Updated: 2026/04/04 12:12:53 by fanilran           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+def ft_garden_name():
+    text = input("Enter garden name: ")
+    print(f"Garden: {text}")
+    print("Status: Growing well!")
